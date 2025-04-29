a228a7fa5cb3100aa047ee3c29683dae3bd666b5
Erika <149546842+ehb2004@users.noreply.github.com>
2025-04-28 23:44:53 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# fantastic-potato-george
+# Running Log App
+
+### Authorship and Attributions
+Created by Erika Beasley for CIS-366.
+- https://getbootstrap.com/docs/5.3/getting-started/introduction/
+- https://www.w3schools.com/html/
+- https://github.com/barrycumbie/jubilant-garbanzo-easy-read
+- https://colorhunt.co/palette/56021f7d1c4ad17d98f4cce9
+- https://forum.bootstrapstudio.io/t/changing-the-navbar-color/10513/3
+
+## User Story
+As a runner, I wanted to be able to log my runs in order to track my progress over the course of months as I train for a half marathon!!
+
+## Narrative
+This running app is a revamped version of my original running log that I created for my first project in CIS-366. It's really come full circle.
+
+Here is a quick look at my original web app ⬇️⬇️⬇️
+![Running Log Preview](images/runLogPreview.png)
+Now look at this code from to OG app! 🙃
+```
+  <h2 id='runHeader'>RUNS</h2>
+  <ul>
+    <li><strong>Jan 27</strong> 2.6 miles, 30m 0s</li>
+    <li><strong>Jan 25</strong> 3.2 miles, 35m 0s</li>
+    <li><strong>Jan 24</strong> 1.2 miles, 13m 48s</li>
+    <li><strong>Jan 21</strong> 2.5 miles, 33m 32s</li>
+    <li><strong>Jan 20</strong> 1 mile, 11m 0s</li>
+    <li><strong>Jan 18</strong> 2.5 miles, 28m 0s</li>
+    <li><strong>Jan 14</strong> 2.9 miles, 40m 0s</li>
+    <li><strong>Jan 9</strong> 3.2 miles, 42m 0s</li>
+  </ul> 
+  ```
+I drew a lot of inspiration from my favorite app, Strava. Take a look! 
+![Strava Web App Screenshot](images/stravaScreenshot.png)
--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
               <div class="card shadow-md h-100">
                 <div class="card-body">
                   <h5 class="card-title">${run.date}</h5>
-                  <p class="card-text"><strong>Distance:</strong> ${run.distance}</p>
+                  <p class="card-text"><strong>Distance:</strong> ${run.distance} miles</p>
                   <p class="card-text"><strong>Time:</strong> ${run.time}</p>
                   <p class="card-text"><strong>Pace:</strong> ${run.pace}</p>
                   <p class="card-text"><strong>Note:</strong> ${run.note}</p>
@@ -32,4 +32,141 @@ $(document).ready(function () {
           container.append(card);
         });
       }
+
+    //   Adding our new runs!!
+    $('#runForm').on('submit', function (event) {
+        event.preventDefault();
+    
+        const newRun = {
+          date: $('#date').val(),
+          distance: $('#distance').val(),
+          time: $('#time').val(),
+          pace: $('#pace').val(),
+          note: $('#note').val()
+        };
+    
+        runData.push(newRun);
+        displayRuns(runData);
+        $(this).trigger('reset');
+      });
+
+       //adding sample run data
+    $('#loadSampleData').on('click', function () {
+        $('#date').val('2025-05-01');
+        $('#distance').val('2.0');
+        $('#time').val('19 minutes 0 seconds');
+        $('#pace').val('8:30 min/mile');
+        $('#note').val('I am super fast!');
+      });
+
+      let deleteIndex = null; // Global to store which run to delete
+
+      // delete run with confirmation modal
+      $('#runEntries').on('click', '.delete-btn', function () {
+          deleteIndex = $(this).data('index'); // Save the index
+          $('#deleteConfirmationModal').modal('show'); // Open the modal
+      });
+      
+      $('#confirmDeleteBtn').on('click', function () {
+          if (deleteIndex !== null) {
+              runData.splice(deleteIndex, 1); // Delete the correct run
+              displayRuns(runData); // Refresh displayed runs
+              deleteIndex = null; // Reset after delete
+              $('#deleteConfirmationModal').modal('hide'); // ✅ Correct one #, not ##
+          }
+      });
+
+      // edit run entries using the add run 
+        $('#runEntries').on('click', '.edit-btn', function () {
+            const index = $(this).data('index');
+            const run = runData[index];
+        
+            $('#date').val(run.date);
+            $('#distance').val(run.distance);
+            $('#time').val(run.time);
+            $('#pace').val(run.pace);
+            $('#note').val(run.note);
+        
+            // Change the form to update mode
+            $('#runForm').off('submit').on('submit', function (event) {
+            event.preventDefault();
+        
+            runData[index] = {
+                date: $('#date').val(),
+                distance: $('#distance').val(),
+                time: $('#time').val(),
+                pace: $('#pace').val(),
+                note: $('#note').val()
+            };
+        
+            displayRuns(runData);
+            $(this).trigger('reset');
+            // Reset the form submission handler
+            resetForm();
+            });
+        });
+
+        function resetForm() {
+            $('#runForm').off('submit').on('submit', function (event) {
+              event.preventDefault();
+          
+              const newRun = {
+                date: $('#date').val(),
+                distance: parseFloat($('#distance').val()).toFixed(2),
+                time: $('#time').val(),
+                pace: $('#pace').val(),
+                note: $('#note').val()
+              };
+          
+              runData.push(newRun);
+              displayRuns(runData);
+              $(this).trigger('reset');
+            });
+          }
+
+      // for exporting runs to console!
+        $('#exportRuns').on('click', function () {
+            const runDataString = JSON.stringify(runData, null, 2);
+            console.log(runDataString);
+        });
+
+// log in 
+  let loggedInUser = null;
+
+// Handle login form submission
+$('#loginForm').on('submit', function(event) {
+  event.preventDefault();
+  
+  const username = $('#username').val().trim();
+  if (username !== '') {
+    loggedInUser = username;
+    $('#loginModal').modal('hide');
+    updateLoginButton();
+    $('#loginForm')[0].reset();
+  }
+});
+
+// Function to update the login/logout button
+function updateLoginButton() {
+  if (loggedInUser) {
+    $('#loginButton')
+      .removeAttr('data-bs-toggle')
+      .removeAttr('data-bs-target')
+      .text(`Well hey there, ${loggedInUser} (Click here to logout)`)
+      .off('click')
+      .on('click', function() {
+        loggedInUser = null;
+        resetLoginButton();
+      });
+  }
+}
+
+// Reset the login button back to Login
+function resetLoginButton() {
+  $('#loginButton')
+    .attr('data-bs-toggle', 'modal')
+    .attr('data-bs-target', '#loginModal')
+    .text('Login')
+    .off('click');
+}
     });
